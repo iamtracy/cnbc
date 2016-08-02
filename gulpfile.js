141abb4fe3f99cc3776adb5689c52715830f2bb5
Tracy Lee <tleecoding@gmail.com>
2016-08-02 12:54:58 -0500

    edits1

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@
     var server = serve.static('app', 3000);
     server.start();
     //use gulp.watch to trigger server actions(notify, start or stop) 
-    gulp.watch(['app/**/*.css', 'app/**/*.html', 'app/**/*.js'], function (file) {
+    gulp.watch(['app/**/*.css', 'app/*.html', 'app/**/*.js'], function (file) {
       server.notify.apply(server, [file]);
     });
   });//end serve
