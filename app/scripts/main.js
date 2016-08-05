8b0d3e165ecbe06fe702e29c367b8ff0fb907bd2
Tracy Lee <tleecoding@gmail.com>
2016-08-05 13:12:00 -0500

    pre-affiliate data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,10 +28,15 @@ jQuery(function(){
 
 $('[data-smooth="smooth"]').on('click', function(event) {
   var target;
+  var theWindow = $(window).width();
   target = this.hash;
   event.preventDefault();
   var navOffset;
-  navOffset = 89;
+  if(theWindow > 420){
+    navOffset = 90
+  } else{
+    navOffset = 126;
+  } 
   return $('html, body').animate({
     scrollTop: $(this.hash).offset().top - navOffset
   }, 650, function() {
